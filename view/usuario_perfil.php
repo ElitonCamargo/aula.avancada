abefea6be4521a4b80dedde00006dc0304941e7f
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-09-14 14:23:54 -0300

    Criando base perfil

--- view/usuario_perfil.php
+++ view/usuario_perfil.php
@@ -1,5 +1,17 @@
 <?php
-    $usuario = $_SESSION['user_logado'];
+$usuario = $_SESSION['user_logado'];
 ?>
+<div class="container">
+    <h1>Seja bem vindo: <?php echo $usuario->nome; ?></h1>
 
-<h1>Seja bem vindo: <?php echo $usuario->nome; ?></h1>
+    <fieldset>
+        <legend>Minha fotos</legend>
+
+        <div class="card-columns">
+            <div class="card">
+                <img class="card-img-top" src="">                
+            </div> 
+        </div>
+        
+    </fieldset>
+</div>
